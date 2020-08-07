03680a4a9d26ea4b6aed8439e6451167d38a00a7
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2020-08-07 14:59:54 +0200

    Added example data

---
+++ inst/Input example.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(scp)
+
+## Create an MaxQuant output example file
+
+## The dataset is a subset of the SCoPE2 dataset (Specht et al. 2019, BioRXiv)
+## https://www.biorxiv.org/content/10.1101/665307v3
+## The datatable was downloaded from https://drive.google.com/drive/folders/1VzBfmNxziRYqayx3SP-cOe2gu129Obgx
+## To run this code, you need to first run scp1.R
+read.csv("extdata/evidence_unfiltered.csv", sep = ",", header = TRUE) %>% 
+  ev %>%
+  select(-c("X", "X1", "lcbatch", "sortday",  "digest")) %>%
+  ## channel naming should be consistent with metadata
+  rename_all(gsub, 
+             pattern = "^Reporter[.]intensity[.](\\d*)$", 
+             replacement = "RI\\1") %>%
+  ## MS set should be consistent with metadata and other data
+  rename(Raw.file = "Set", 
+         modseq = "peptide",
+         Leading.razor.protein = "protein") %>%
+  ## Remove "X" at start of batch 
+  mutate(Set = gsub("^X", "", Set)) %>%
+  ## keep only single cell runs 
+  filter(!grepl("col\\d{2}|arrier|Ref|Master|SQC|blank", Set)) %>%
+  ## remove experimental sets concurrent with low mass spec performance
+  filter(!grepl("FP9[56]|FP103", Set)) %>%
+  ## keep only sets selected in scp1
+  filter(Set %in% names(scp1)) ->
+  mqFile
+format(object.size(mqFile), units = "MB", digits = 2)
+save(mqFile, file = file.path("data/mqFile.rda"), 
+     compress = "xz", compression_level = 9)
+
+## Create the associated annotation file
+## The datatables are downloaded from https://www.biorxiv.org/content/10.1101/665307v3
+
+cells <- read.csv("extdata/annotation.csv", check.names = F)
+batch <- read.csv("extdata/batch.csv", check.names = F)
+## Clean the sample metadata so that it meets the requirements for
+## `scp::readSCP`. The cell annotation and batch annotation are merge into a 
+## table
+inner_join(x = cells %>% 
+             pivot_longer(-Set, 
+                          names_to = "Channel", 
+                          values_to = "SampleAnnotation") %>%
+             mutate_all(as.character), 
+           y = batch %>% 
+             rename(set = "Set") %>%
+             mutate_all(as.character),
+           by = "Set") -> sampleAnnotation
+format(object.size(sampleAnnotation), units = "MB", digits = 2)
+save(sampleAnnotation, file = file.path("data/sampleAnnotation.rda"), 
+     compress = "xz", compression_level = 9)
+
+
--- inst/scp1.R
+++ inst/scp1.R
@@ -8,11 +8,12 @@
 library(scp)
 library(scpdata)
 library(tidyverse)
+library(QFeatures)
 data("specht2019v2")
 
 ## Randomly sample 1 SCoPE2 set from each possible combination of batch variables
 set.seed(1000)
-specht2019v2 %>%
+specht2019v2[, , 1:177] %>%
   colData %>%
   data.frame %>%
   group_by(lcbatch, sortday, digest) %>%
@@ -21,24 +22,34 @@ specht2019v2 %>%
   sampledRuns
 scp1 <- specht2019v2[, , sampledRuns]
 
-## Sample 150 proteins from each run
+## Sample 100 proteins from each run
 set.seed(1000)
-rowDataToDF(scp1, seq_along(scp1), "protein") %>%
+rowDataToDF(scp1, seq_along(scp1), c("peptide", "protein")) %>%
   data.frame %>% 
   rownames_to_column("psm") %>%
+  group_by(peptide) %>%
+  mutate(nprots = length(unique(protein))) %>%
+  filter(nprots == 1) %>%
   group_by(.assay) %>%
-  filter(protein %in% sample(unique(protein), 150)) ->
+  filter(protein %in% sample(unique(protein), 100)) ->
   l
 l <- split(l$psm, f = l$.assay)
 scp1 <- scp1[l, ]
 
-## Remove all place holder samples
-lapply(experiments(scp1), function(e) colSums(is.na(assay(e))) != nrow(e) ) %>%
-  unname %>%
-  unlist -> 
-  cols
-cols <- names(cols)[cols]
-scp1 <- scp1[, cols, ]
+## Aggregate to peptides
+scp1 <- aggregateFeaturesOverAssays(scp1, i = names(scp1), fcol = "peptide",
+                                    name = paste0("pep_", names(scp1)),
+                                    fun = colMeans, na.rm = TRUE)
+
+## Join peptides 
+scp1 <- joinAssays(scp1, i = grep("pep_", names(scp1)), name = "peptides")
+
+## Aggregate to proteins
+scp1 <- aggregateFeatures(scp1, i = "peptides", fcol = "protein",
+                          name = "proteins", fun = colMedians, na.rm = TRUE)
+
+## Keep only the PSM, joined peptide and protein data
+scp1 <- scp1[, , !grepl("pep_", names(scp1))]
 
 ## Save the data 
 format(object.size(scp1), units = "MB", digits = 2)
