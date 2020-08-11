7b0fff27d7dec5b44ba31de087874e2f2fe7d690
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2020-08-11 18:32:12 +0200

    Added unit tests
    Former-commit-id: 6060d1c15122260a6dc452ec02ddad6eefb607db
    Former-commit-id: 295b22952f6cec97c746628a3530a771d45699f9

---
+++ tests/testthat/test-readSCP.R
@@ -0,0 +1,35 @@
+data("mqFile")
+data("sampleAnnotation")
+
+
+test_that("readSCP: correct use", {
+    ## Multiple batches 
+    scp <- readSCP(mqFile, sampleAnnotation, batchCol = "Set", 
+                   channelCol = "Channel")
+    expect_identical(dims(scp),
+                     matrix(c(334L, 433L, 321L, rep(16L, 3)), byrow = TRUE, nrow = 2, 
+                            dimnames = list(NULL, c("190222S_LCA9_X_FP94BM", "190321S_LCA10_X_FP97AG", "190914S_LCB3_X_16plex_Set_21"))))
+    ## Single batch
+    scp <- readSCP(mqFile %>% 
+                     dplyr::filter(Set == "190222S_LCA9_X_FP94BM"), 
+                   sampleAnnotation, batchCol = "Set", 
+                   channelCol = "Channel")
+    expect_identical(dims(scp),
+                     matrix(c(334L, 16L), nrow = 2, 
+                            dimnames = list(NULL, "190222S_LCA9_X_FP94BM")))
+    
+})
+
+test_that("readSCP: warnings", {
+  ## Missing batch in metadata 
+  expect_warning(scp <- readSCP(mqFile, 
+                                sampleAnnotation  %>% 
+                                  dplyr::filter(Set == "190222S_LCA9_X_FP94BM"), 
+                                batchCol = "Set", 
+                                channelCol = "Channel"),
+                 regexp = "Missing metadata. The features are removed")
+  expect_identical(dims(scp),
+                   matrix(c(334L, 16L), nrow = 2, 
+                          dimnames = list(NULL, "190222S_LCA9_X_FP94BM")))
+  
+})
