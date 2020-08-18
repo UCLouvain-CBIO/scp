9d263a97476e831b46eaaced3b8d2436f8895dc6
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-08-18 22:48:01 +0200

    fix: fixing several checking warnings and errors
    Former-commit-id: dc9f16478524a084070bf10e685897ea31f20815 [formerly c5963a3b1f9c5fa2d89f47b6e5ce8a110a617d10]
    Former-commit-id: dc9f16478524a084070bf10e685897ea31f20815
    Former-commit-id: ff96473dc841d7b4391bf334f707a12c38dd397e

--- R/readSCP.R
+++ R/readSCP.R
@@ -1,52 +1,53 @@
 ##' @import SingleCellExperiment
 ##' @import QFeatures
-##' @importFrom utils read.csv
 ##' @import tidyverse
 
 
-##' @title Read single-cell proteomics data as a QFeatures object from tabular 
-##' data and metadata
+##' @title Read single-cell proteomics data as a QFeatures object from
+##'     tabular data and metadata
 ##'
 ##' @description
 ##'
-##' Convert tabular quantitative MS data and metadata from a spreadsheet or a 
-##' `data.frame` into a `QFeatures` object containing `SingleCellExperiment` 
-##' objects. 
+##' Convert tabular quantitative MS data and metadata from a
+##' spreadsheet or a `data.frame` into a [QFeatures] object containing
+##' [SingleCellExperiment] objects.
 ##'
-##' @param quantTable File or object holding the quantitative data. Can be
-##'     either a `character(1)` with the path to a text-based
-##'     spreadsheet (comma-separated values by default, but see `...`)
-##'     or an object that can be coerced to a `data.frame`. It is
-##'     advised not to encode characters as factors.
+##' @param quantTable File or object holding the quantitative
+##'     data. Can be either a `character(1)` with the path to a
+##'     text-based spreadsheet (comma-separated values by default, but
+##'     see `...`) or an object that can be coerced to a
+##'     `data.frame`. It is advised not to encode characters as
+##'     factors.
 ##' 
-##' @param metaTable A `data.frame` or any object that can be coerced to a 
-##'     `data.frame`. `metaTable` is expected to contains all the sample meta 
-##'     information. Required fields are the acquisition batch (given by 
-##'     `batchCol`) and the acquisition channel within the batch (e.g. TMT 
-##'     channel, given by `channelCol`). Additional fields (e.g. sample type, 
-##'     acquisition date,...) are allowed and will be stored as sample meta data.
+##' @param metaTable A `data.frame` or any object that can be coerced
+##'     to a `data.frame`. `metaTable` is expected to contains all the
+##'     sample meta information. Required fields are the acquisition
+##'     batch (given by `batchCol`) and the acquisition channel within
+##'     the batch (e.g. TMT channel, given by
+##'     `channelCol`). Additional fields (e.g. sample type,
+##'     acquisition date,...) are allowed and will be stored as sample
+##'     meta data.
 ##'
-##' @param batchCol A `numeric(1)` or `character(1)` pointing to the column of 
-##'     `quantTable` and `metaTable` that contain the batch names. Make sure 
-##'     that the column name in both table are either identical (if you supply a
-##'     `character`) or have the same index (if you supply a `numeric`).
+##' @param batchCol A `numeric(1)` or `character(1)` pointing to the
+##'     column of `quantTable` and `metaTable` that contain the batch
+##'     names. Make sure that the column name in both table are either
+##'     identical (if you supply a `character`) or have the same index
+##'     (if you supply a `numeric`).
 ##' 
-##' @param channelCol A `numeric(1)` or `character(1)` pointing to the column of
-##'     `metaTable` that contains the column names of the quantitive data in 
-##'     `quantTable` (see Example).
+##' @param channelCol A `numeric(1)` or `character(1)` pointing to the
+##'     column of `metaTable` that contains the column names of the
+##'     quantitive data in `quantTable` (see Example).
 ##' 
-##' 
-##' @param verbose A `logical(1)` indicating whether the progress of the data 
-##'     reading and formatting should be printed to the console. 
+##' @param verbose A `logical(1)` indicating whether the progress of
+##'     the data reading and formatting should be printed to the
+##'     console. Default is `TRUE`.
 ##' 
 ##' @param ... Further arguments that can be passed on to [read.csv]
 ##'     except `stringsAsFactors`, which is always `FALSE`.
 ##'
-##' @details 
-##' 
-##'
-##' @return An instance of class [QFeatures]. The expression data of each batch 
-##'     is stored in a separate assay as a `SingleCellExperiment` object.
+##' @return An instance of class [QFeatures]. The expression data of
+##'     each batch is stored in a separate assay as a
+##'     [SingleCellExperiment] object.
 ##'
 ##' @author Laurent Gatto, Christophe Vanderaa
 ##' 
@@ -59,6 +60,7 @@
 ##' 
 ##' ## Load an example table containing MaxQuant output
 ##' data("mqFile")
+##' 
 ##' ## Load the (user-generated) annotation table
 ##' data("sampleAnnotation")
 ##' 
@@ -76,7 +78,7 @@ readSCP <- function(quantTable,
                     ...) {
     metaTable <- as.data.frame(metaTable)
     ## Create the SingleCellExperiment object
-    if (verbose) cat("Loading data as a 'SingleCellExperiment' object\n")
+    if (verbose) message("Loading data as a 'SingleCellExperiment' object")
     ecol <- unique(metaTable[, channelCol])
     scp <- readSingleCellExperiment(table = quantTable, 
                                     ecol = ecol, 
@@ -94,11 +96,11 @@ readSCP <- function(quantTable,
     }
     
     ## Split the SingleCellExperiment object by batch column
-    if (verbose) cat(paste0("Splitting data based on '", batchCol, "'\n"))
+    if (verbose) message(paste0("Splitting data based on '", batchCol))
     scp <- .splitSCE(scp, f = batchCol)
     
     ## Add unique sample identifiers
-    if (verbose) cat(paste0("Formating sample metadata (colData)\n"))
+    if (verbose) message(paste0("Formating sample metadata (colData)"))
     for (i in 1:length(scp)) {
         colnames(scp[[i]]) <- paste0(names(scp)[[i]], "_", colnames(scp[[i]]))
     }
@@ -108,8 +110,9 @@ readSCP <- function(quantTable,
                                   metaTable[, channelCol])
     cd <- cbind(cd, metaTable[rownames(cd), ])
     
-    ## Store the data as a QFeatures object and add the experimental information
-    if (verbose) cat("Formating data as a 'QFeatures' object\n")
+    ## Store the data as a QFeatures object and add the experimental
+    ## information
+    if (verbose) message("Formating data as a 'QFeatures' object")
     QFeatures(experiments = scp, 
               colData = cd)
 }
@@ -149,19 +152,21 @@ readSCP <- function(quantTable,
 ##'
 ##'
 ##' @md
+##' 
 ##' @export
-##' 
+##'
+##' @importFrom methods as
+##'
 ##' @examples 
-##' 
-##' 
 ##' ## Load a data.frame with PSM-level data
 ##' data(mqFile)
+##' 
 ##' ## Create the QFeatures object
 ##' sce <- readSingleCellExperiment(mqFile, grep("RI", colnames(mqFile)))
 readSingleCellExperiment <- function(table, 
                                      ecol, 
                                      fnames,
-                                     ...){
+                                     ...) {
     ## Read data as SummarizedExperiment
     sce <- readSummarizedExperiment(table, ecol, fnames, ...)
     sce <- as(sce, "SingleCellExperiment")
@@ -169,26 +174,28 @@ readSingleCellExperiment <- function(table,
 }
 
 
-# Split SingleCellExperiment into an ExperimentList
-#
-# The fonction creates an [ExperimentList] containing [SingleCellExperiment] 
-# objects from a [SingleCellExperiment] object. `f` is used to split `x`` along 
-# the rows (`f`` was a feature variable name) or samples/columns (f was a 
-# phenotypic variable name). If f is passed as a factor, its length will be 
-# matched to nrow(x) or ncol(x) (in that order) to determine if x will be split 
-# along the features (rows) or sample (columns). Hence, the length of f must 
-# match exactly to either dimension.
-# 
-# This function is not exported. if this is needed, create a pull request to 
-# `rformassspectrometry/QFeatures`.
-# 
-# @param x a single [SingleCellExperiment] object 
-# @param f a factor or a character of length 1. In the latter case, `f`` will 
-#     be matched to the row and column data variable names (in that order). If 
-#     a match is found, the respective variable is extracted, converted to a 
-#     factor if needed
-#
-.splitSCE <- function(x, f){
+##' Split SingleCellExperiment into an ExperimentList
+##'
+##' The fonction creates an [ExperimentList] containing
+##' [SingleCellExperiment] objects from a [SingleCellExperiment]
+##' object. `f` is used to split `x`` along the rows (`f`` was a feature
+##' variable name) or samples/columns (f was a phenotypic variable
+##' name). If f is passed as a factor, its length will be matched to
+##' nrow(x) or ncol(x) (in that order) to determine if x will be split
+##' along the features (rows) or sample (columns). Hence, the length of
+##' f must match exactly to either dimension.
+##' 
+##' This function is not exported. If this is needed, create a pull
+##' request to `rformassspectrometry/QFeatures`.
+##' 
+##' @param x a single [SingleCellExperiment] object
+##' 
+##' @param f a factor or a character of length 1. In the latter case,
+##'     `f` will be matched to the row and column data variable names
+##'     (in that order). If a match is found, the respective variable
+##'     is extracted, converted to a factor if needed
+##' @noRd
+.splitSCE <- function(x, f) {
     ## Check that f is a factor
     if (is.character(f)) {
       if (length(f) != 1) 
