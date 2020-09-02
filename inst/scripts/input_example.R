6b1301fb4177c4a7576f4bced36c4f4e7b45420b
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2020-09-02 10:12:58 +0200

    feat: added blank sample containing few features (see issue #9)
    Former-commit-id: dbf38e3a6fc3883e33fbcbe7fa79063cfd1768d6

--- inst/scripts/input_example.R
+++ inst/scripts/input_example.R
@@ -8,7 +8,9 @@ library(scp)
 ## datatable was downloaded from
 ## https://drive.google.com/drive/folders/1VzBfmNxziRYqayx3SP-cOe2gu129Obgx
 ## To run this code, you need to first run scp1.R
-read.csv("inst/extdata/evidence_unfiltered.csv", sep = ",", header = TRUE) %>% 
+ev <- read.csv("inst/extdata/evidence_unfiltered.csv", sep = ",", header = TRUE) 
+  
+ev %>% 
   select(-c("X", "X1", "lcbatch", "sortday",  "digest")) %>%
   ## channel naming should be consistent with metadata
   rename_all(gsub, 
@@ -20,14 +22,11 @@ read.csv("inst/extdata/evidence_unfiltered.csv", sep = ",", header = TRUE) %>%
                 protein = "Leading.razor.protein") %>%
   ## Remove "X" at start of batch 
   mutate(Set = gsub("^X", "", Set)) %>%
-  ## keep only single cell runs 
-  filter(!grepl("col\\d{2}|arrier|Ref|Master|SQC|blank", Set) &
-           ## remove experimental sets concurrent with low mass spec performance
-           !grepl("FP9[56]|FP103", Set) &
-           ## keep only sets selected in scp1
-           Set %in% names(scp1) &
+  ## keep sets selected in scp1
+  filter(Set %in% names(scp1) &
            ## keep only a few proteins
-           protein %in% rowDataToDF(scp1, 1:3, "protein")$protein) ->
+           protein %in% rowDataToDF(scp1, 1:3, "protein")$protein |
+           grepl("FP97_blank_01", Set) ) ->
   mqScpData
 format(object.size(mqScpData), units = "MB", digits = 2)
 save(mqScpData, file = file.path("data/mqScpData.rda"), 
@@ -58,10 +57,17 @@ inner_join(x = cells %>%
              dplyr::rename(Set = set) %>%
              mutate_all(as.character),
            by = "Set") %>%
-  filter(Set %in% mqScpData$Set) -> 
+  filter(Set %in% mqScpData$Set) %>%
+  ## Add the metadata for the blank sample
+  add_row(Set = grep("blank", mqScpData$Set, value = TRUE) %>%
+            unique %>%
+            rep(16),
+          Channel = paste0("RI", 1:16),
+          SampleType = "Blank",
+          lcbatch = "LCA10",
+          sortday = "s8") ->
   sampleAnnotation
+  
 format(object.size(sampleAnnotation), units = "MB", digits = 2)
 save(sampleAnnotation, file = file.path("data/sampleAnnotation.rda"), 
      compress = "xz", compression_level = 9)
-
-
--- tests/testthat/test-readSCP.R
+++ tests/testthat/test-readSCP.R
@@ -4,11 +4,18 @@ data("sampleAnnotation")
 
 test_that("readSCP: correct use", {
     ## Multiple batches 
-    scp <- readSCP(mqScpData, sampleAnnotation, batchCol = "Set", 
+    scp <- readSCP(mqScpData, 
+                   sampleAnnotation, 
+                   batchCol = "Set", 
                    channelCol = "Channel")
-    expect_identical(dims(scp),
-                     matrix(c(334L, 433L, 321L, rep(16L, 3)), byrow = TRUE, nrow = 2, 
-                            dimnames = list(NULL, c("190222S_LCA9_X_FP94BM", "190321S_LCA10_X_FP97AG", "190914S_LCB3_X_16plex_Set_21"))))
+    expect_identical(dims(scp)[, unique(mqScpData$Set)],
+                     matrix(c(334L, 433L, 109L, 321L, rep(16L, 4)), byrow = TRUE, nrow = 2, 
+                            dimnames = list(NULL, c("190222S_LCA9_X_FP94BM", "190321S_LCA10_X_FP97AG", "190321S_LCA10_X_FP97_blank_01", "190914S_LCB3_X_16plex_Set_21"))))
+    ## Note: there is something weird in this test... When executing the code locally, 
+    ## the assays in `scp` are ordered alphabetically, but when runned by 
+    ##  `devtools::test()`, the assays are ordered as they appear in `mqScpData`
+    ## hence the need for `dims(scp)[, unique(mqScpData$Set)]` to avoid an error
+    
     ## Single batch
     scp <- readSCP(mqScpData %>% 
                      dplyr::filter(Set == "190222S_LCA9_X_FP94BM"), 
