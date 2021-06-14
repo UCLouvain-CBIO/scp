f479a9834724c58581c4c9863470563e74dae6b2
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2021-06-14 15:24:32 +0200

    refactor: deprecated rowDataToDF in favor of rbindRowData
    Former-commit-id: f57947609259c1b690ffae75c8107ea111217fa8 [formerly ed5b9085a8b15a4335c38a2df072d1c2cbcff497]
    Former-commit-id: f57947609259c1b690ffae75c8107ea111217fa8
    Former-commit-id: 64beec7ef6b8880c646815148a908d138ac7310f

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,21 +1,8 @@
 data("scp1")
 
 test_that("rowDataToDF", {
-    ## Correct use 
-    ## single assay
-    test <- rowDataToDF(scp1, i = "190321S_LCA10_X_FP97AG", vars = "peptide")
-    expect_identical(nrow(test), nrow(scp1[[1]]))
-    expect_identical(dimnames(test), list(rownames(scp1[[1]]), c("peptide", ".assay", ".rowname")))
-    ## Multiple assays
-    test <- rowDataToDF(scp1, i = 1:3, vars = "peptide")
-    expect_identical(nrow(test),
-                     as.integer(sum(dims(scp1)[1, 1:3])))
-    expect_identical(dimnames(test),
-                     list(unlist(lapply(1:3, function(x) rownames(scp1[[x]]))),
-                          c("peptide", ".assay", ".rowname")))
-    ## Variable not found
-    expect_error(rowDataToDF(scp1, i = 1:3, vars = "foo"),
-                 regexp = "rowData variable\\(s\\) not found")
+    expect_warning(rowDataToDF(scp1, i = 1),
+                   regexp = "deprecated")
 })
 
 test_that("aggregateFeaturesOverAssays", {
